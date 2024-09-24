c314f3d3871d195e9094d433cad85b1cf18c6ebc
lepau <lepaupitz@gmail.com>
2024-09-24 14:52:04 -0300

    ajeitando

--- src/main/java/com/example/Authenticator/GraphController.java
+++ src/main/java/com/example/Authenticator/GraphController.java
@@ -1,12 +1,12 @@
 package com.example.Authenticator;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/graph")
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
 public class GraphController {
 
     @Autowired
@@ -20,4 +20,5 @@ public class GraphController {
             return e.getMessage();
         }
     }
+
 }
